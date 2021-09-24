244e46acaf5bc61189922e8bd7c3eb23778e86c7
Charles Dantas <72893967+charlesdev771@users.noreply.github.com>
2021-09-24 16:58:36 -0300

    PIS 2.0

--- PIS/src/Workstation.sh
+++ PIS/src/Workstation.sh
@@ -10,10 +10,10 @@
 
     #Bases of system
       apt install arc arj cabextract lhasa p7zip p7zip-full p7zip-rar rar unrar unace unzip xz-utils zip -y; apt install snapd -y;
-      apt install nvidia-driver-435 -y; apt install synaptic -y; apt install gparted -y; apt install qbittorrent -y;
-      dpkg --add-architecture i386; apt update; apt install aptitude build-essential most cups-pdf poppler-utils -y;
-      apt install net-tools -y; apt install git -y; apt install gufw -y; apt install pv -y;  apt install postgresql -y; apt install mongodb -y;
-      apt install virtualbox -y; apt-get install apt-transport-https;
+      apt install nvidia-driver-450 -y; apt install synaptic -y; apt install gparted -y; apt install qbittorrent -y;
+      dpkg --add-architecture i386 -y; apt update; apt install aptitude build-essential most cups-pdf poppler-utils -y;
+      apt install net-tools -y; apt install git -y; apt install gufw -y; apt install pv -y;
+      apt install virtualbox -y; apt-get install apt-transport-https -y;
       export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
     #;
 
@@ -30,26 +30,28 @@
       apt install kdenlive -y; apt install simplescreenrecorder -y; apt install vlc -y;
     #;
 
-    #Install langs c,c++,c#,python,perl, java, kotlin,dart,ruby, assembly and tools
+    #Install langs c,c++,c#,python,perl, java, kotlin, dart, ruby, assembly and tools
       apt install gcc -y; apt install g++;  wget -c https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-      sudo dpkg -i packages-microsoft-prod.deb; sudo apt-get install aspnetcore-runtime-3.1; apt update; sudo apt-get install dotnet-runtime-3.1;
+      sudo dpkg -i packages-microsoft-prod.deb; sudo apt-get install aspnetcore-runtime-3.1; apt update; sudo apt-get install dotnet-runtime-3.1 -y;
       apt install python3 -y; apt update; sudo apt-get install dotnet-sdk-3.1 apt install perl -y; sudo apt-get install openjdk-8-jdk -y;
       apt install default-jre; snap install --classic kotlin;
       sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -';
+      apt install postgresql -y; apt install mongodb -y
       sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list';
-      sudo apt-get update;  sudo apt-get install dart; export PATH="$PATH:/usr/lib/dart/bin";
-      echo 'export PATH="$PATH:/usr/lib/dart/bin"' >> ~/.profile; apt install ruby; apt install nasm;
+      sudo apt-get update;  sudo apt-get install dart -y; export PATH="$PATH:/usr/lib/dart/bin";
+      echo 'export PATH="$PATH:/usr/lib/dart/bin"' >> ~/.profile; apt install ruby -y; apt install nasm -y;
+
     #;
 
     #Ides, editors
-      apt install codeblocks; -y; apt apt install idle3 -y; sudo snap install android-studio --classic;
+      apt install codeblocks -y; apt apt install idle3 -y; sudo snap install android-studio --classic;
       apt install netbeans -y; snap install code --classic; snap install intellij-idea-community --classic --edge;
       snap install atom
     #;;
 
     #Install frameworks
-      apt install nodejs; apt install npm; npm install -g react-cli; npm install -g react-native-cli;
-      npm install -g expo-cli apt install composer -y;
+      apt install nodejs -y; apt install npm -y; npm install -g react-cli; npm install -g react-native-cli;
+      npm install -g expo-cli; npm install -g sass; apt install composer -y;
     #;
 
     #Confing to android
